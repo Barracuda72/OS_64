07d0a7e0b1194b29663d1268bea9331d87dfdcd8
Barracuda72 <barracuda72@bk.ru>
2013-07-29 22:46:31 +0600

    One step closer to malloc

--- mm/mem.c
+++ mm/mem.c
@@ -1,11 +1,16 @@
 #include <mem.h>
 
+kmem_header *heap;
+
 void mem_init(unsigned long pl_addr, unsigned long pl_size)
 {
-        unsigned long last_phys_page = (pl_addr&0x0FFFF000) + 0x1000;
-        printf("Last phys page is %l\n", last_phys_page);
-        page_init(&last_phys_page);
-	phys_init(&last_phys_page);
+	// last_page - последняя страница как физической,
+	// так и виртуальной памяти
+        unsigned long last_page = (pl_addr&0x0FFFF000) + 0x1000;
+	heap = (kmem_header *)(last_page|0xFFFFFFFFC0000000);
+        printf("Last phys page is %l\n", last_page);
+        page_init(&last_page);
+	phys_init(&last_page);
 }
 
 void *kmalloc(unsigned long size)
