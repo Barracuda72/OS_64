65c298e38c89c85d34cf5e3cd9e2ec2ccff64d55
Barracuda72 <barracuda72@bk.ru>
2013-08-05 04:11:38 +0600

    kernel_main cleanup

--- kernel.c
+++ kernel.c
@@ -29,28 +29,10 @@ long kernel_start(unsigned long mb_magic, multiboot_info_t *mb)
         // Общая информация о памяти
         printf ("mem_lower = %dKB, mem_upper = %dKB\n",
                (unsigned int) mb->mem_lower, (unsigned int) (mb->mem_upper));
-/*
-        // Конкретная карта памяти
-        mmap_info_t *mmap;
-
-        printf ("mmap_addr = 0x%X, mmap_length = 0x%X\n",
-                 (unsigned int) mb->mmap_addr, (unsigned int) mb->mmap_length);
-        for (mmap = (mmap_info_t *) (mb->mmap_addr);
-              (unsigned long) mmap < (mb->mmap_addr + mb->mmap_length);
-              mmap = (mmap_info_t *) ((unsigned long) mmap
-                       + mmap->size + sizeof (mmap->size)))
-             printf (" size = 0x%x, base_addr = 0x%l,"
-                     " length = 0x%l, type = 0x%x\n",
-                     (unsigned int) mmap->size,
-                     (unsigned long) mmap->base_addr,
-                     (unsigned long) mmap->length,
-                     (unsigned int) mmap->type);
-*/
 	intr_init();
 
 	ktty_puts("Welcome to 64-bit OS written in C!\n");
 	printf("OS build date %s %s\n", __DATE__, __TIME__);
-	//ktty_puts("Have a nice day!\n");
 /*	
 	printf("CPU vendor: %s\n", CPU_get_vendor_string());
 	printf("CPU Family: %i\n", CPU_get_info_family());
@@ -68,14 +50,13 @@ long kernel_start(unsigned long mb_magic, multiboot_info_t *mb)
 	smp_init();
 	extern unsigned long kernel_end;
 	unsigned long pool = &kernel_end;
-	mem_init(pool, (mb->mem_upper>>10)+2);	// TODO: Исправить!
+	mem_init(pool, (mb->mem_upper>>10)+2, mb);	// TODO: Исправить!
 
 	// Тест сразу подсистем физической и логической памяти
 	unsigned char *new_video = 0x7ABEFAEDF000;	// Совершенно левый адрес
 	mount_page(0xB8000, new_video);
-	new_video[0] = 'T';
-	printf("MB = %l\n", mb);
-	
+	new_video[0] = 'Q';
+	umount_page(new_video);
 	for(;;) asm("hlt");
 	return 0;
 }
