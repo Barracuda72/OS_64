2effbdfbd7d15d2086a5f3cf8933a78811175f58
Barracuda72 <barracuda72@bk.ru>
2013-08-07 02:06:38 +0600

    Kernel heap support in PF handler

--- mm/pagefault.h
+++ mm/pagefault.h
@@ -3,45 +3,53 @@
 
 #include <intr.h>
 #include <klibc.h>
+#include <phys.h>
+#include <page.h>
 
 #include <debug.h>
-/*
-IRQ_HANDLER(page_fault)
-{
-	BREAK();
-        unsigned long addr,err,cs,rip;
-	asm("pop %0 \n pop %1":"=a"(err),"=b"(rip));
-        asm("mov %%cr2, %0":"=a"(addr));
-        printf("Page fault: %l\n",addr);
-	printf("Flags: %b\n", err);
-	printf("Caused by %l\n", rip);
-        for(;;) asm("hlt");
-}
-*/
+
+#define PF_PRESENT (1<<0)
+#define PF_WRITE   (1<<1)
+#define PF_USER    (1<<2)
 
 void page_fault(void);	// IRQ handler
 asm("page_fault: \n \
+ push %rax \n \
  push %rdi \n \
  push %rsi \n \
  push %rdx \n \
- mov 0x18(%rsp), %rdi \n \
- mov 0x20(%rsp), %rdx \n \
+ mov 0x20(%rsp), %rdi \n \
+ mov 0x28(%rsp), %rdx \n \
  mov %cr2, %rsi \n \
  call _page_fault \n \
  pop %rdx \n \
  pop %rsi \n \
  pop %rdi \n \
- sub $0x10, %rsp \n \
  movb $0x20, %al \n \
  outb %al, $0x20 \n \
+ pop %rax \n \
+ add $0x8, %rsp \n \
  iretq \n \
 ");
 
-void _page_fault(unsigned long errcode, unsigned long addr, unsigned long rip)
+// Адрес структуры кучи ядра
+extern unsigned long kernel_heap;
+
+void _page_fault(unsigned char errcode, unsigned long addr, unsigned long rip)
 {
-        printf("Page fault: %l\n",addr);
-        printf("Flags: %b\n", errcode);
-        printf("Caused by %l\n", rip);
+        printf("PF: a = %l, f = %b, rip = %l\n", 
+	  addr, errcode, rip);
+
+	// Если произошла ошибка при обращении к несуществующей странице
+	// внутри кучи ядра, выделим страницу
+	if ((addr >= kernel_heap) && 
+	   (!(errcode&PF_PRESENT) && !(errcode&PF_USER)))
+	{
+	  printf("Kernel heap fault, recovering...\n");
+	  mount_page(alloc_phys_page(), addr&0xFFFFFFFFFFFFF000L);
+	  //BREAK();
+	  return;
+	}
         for(;;) asm("hlt");
 }
 
