b6527aa504344924f8105fb9c82c1bb2af2398fa
Barracuda72 <barracuda72@bk.ru>
2016-02-12 20:34:25 +0500

    Исследование работы динамического компоновщика

---
+++ elf/test2.c
@@ -0,0 +1,15 @@
+//#include <unistd.h>
+#include "../kernel/debug.h"
+#include "../libc/os64sys.h"
+
+char hello[] = "Hello, world!\n";
+
+int main(int argc, char *argv[])
+{
+  BREAK();
+  int pid = getpid();
+  //kprintf("My PID is %d!\n", pid);
+  write(1, hello, sizeof(hello));
+  return 0;
+}
+
---
+++ elf/vld.c
@@ -0,0 +1,12 @@
+/*
+ * Тестовая площадка для компоновщика
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  printf("argv = %s\n",argv[0]);
+  return 0;
+}
